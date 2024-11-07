8d61b61e9cf7fd5e2e9a9c22d546052fa492f918
ronyislam502 <ronyislam502@gmail.com>
2024-11-07 15:36:05 +0600

    editPost

---
+++ src/components/DeletePost.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { Button } from "@nextui-org/button";
+import { useState } from "react";
+import { MdDeleteOutline } from "react-icons/md";
+import { toast } from "sonner";
+
+import { useDeletePostMutation } from "../redux/features/post/postApi";
+import { TError } from "../types/global";
+
+import CustomModal from "./ui/CustomModal";
+
+const DeletePost = ({ id }: { id: string }) => {
+  const [isDeletePostModalOpen, setIsDeletePostModalOpen] = useState(false);
+
+  const [deleteNow] = useDeletePostMutation();
+
+  const handleSubmit = async () => {
+    const toastId = toast.loading("Post deleting please wait!");
+
+    try {
+      const res = await deleteNow(id).unwrap();
+
+      setIsDeletePostModalOpen(true);
+      toast.success(res.message, { id: toastId, duration: 2000 });
+    } catch (error) {
+      console.log(error);
+      const err = error as TError;
+
+      toast.error(err.data.message || "Something went wrong", {
+        id: toastId,
+        duration: 2000,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <Button
+        isIconOnly
+        className=""
+        onClick={() => {
+          setIsDeletePostModalOpen(true);
+        }}
+      >
+        <MdDeleteOutline className="text-lg" />
+      </Button>
+      <CustomModal
+        footer={false}
+        isOpen={isDeletePostModalOpen}
+        title="Delete Post"
+        onClose={() => setIsDeletePostModalOpen(false)}
+      >
+        <div className="flex flex-col justify-center items-center pb-4 space-y-4">
+          <p>Are you sure?</p>
+          <Button
+            className=""
+            onClick={() => {
+              handleSubmit();
+            }}
+          >
+            Deelete
+          </Button>
+        </div>
+      </CustomModal>
+    </div>
+  );
+};
+
+export default DeletePost;
